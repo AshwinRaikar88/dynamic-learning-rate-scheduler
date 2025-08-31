363c3c12f1e066776198233515a56861c48a0192
Ashwin Raikar <ashwin.raikar77@gmail.com>
2025-08-31 12:04:33 -0400

    Added training files to the repository

---
+++ image-classification/MNIST experiments/utils/dlrs.py
@@ -0,0 +1,96 @@
+import numpy as np
+
+class DLRS:
+    def __init__(self, lr_upper_threshold=0.1, lr_lower_threshold=1e-7, delta_increase=0.5, delta_decrease=1):
+        self.lr_upper_threshold = lr_upper_threshold
+        self.lr_lower_threshold = lr_lower_threshold
+
+        self.del_i = delta_increase
+        self.del_d = delta_decrease
+
+    def decrease_ut(self):
+        self.lr_upper_threshold -= self.lr_upper_threshold/2
+        return self.lr_upper_threshold
+
+    def step(self, lr, loss_metrics):
+        # Get the order of the learning rate
+        order_lr = np.math.floor(np.math.log10(lr))  
+        # print(f"Old lr = {lr}, lr order = {order_lr}")
+        
+        # Calculate mean of a batch of loss values
+        Lm = np.mean(loss_metrics)
+
+        # Calculate the normalized slope - Observations
+        Norm = (loss_metrics[-1] - loss_metrics[0]) / Lm
+        # print(f"Obs = {Norm} ")
+
+        if Norm == 0:
+            return lr, True
+
+        if Norm > 1e-8: # this means, if Norm is > 0
+            # This means the loss increased so decrease the lr          
+            Norm *= self.del_d * np.math.pow(10, order_lr)
+        else:
+            # This means the loss is decreasing so gradually increase the lr
+            Norm *= self.del_i * np.math.pow(10, order_lr)
+
+        # update learning rate
+        lr -= Norm
+        # print(f"Norm = {Norm} \n Learning rate= {lr}")
+        lr = abs(lr)
+
+        if lr < self.lr_lower_threshold:
+            lr = self.lr_lower_threshold
+            # print(f"DLRS [info]: Lower limit reached, lr = {lr}")
+            return lr, True
+        elif lr > self.lr_upper_threshold:
+            lr = self.lr_upper_threshold
+            # print(f"DLRS [info]: Upper limit reached, lr = {lr}")
+            return lr, True
+        else:
+            order_lr = np.math.floor(np.math.log10(lr))
+            # print(f"DLRS [info]: New lr = {lr}, lr order = {order_lr}")
+            return lr, True
+
+
+    def step_2(self, lr, loss_metrics):
+        # Get the order of the learning rate
+        order_lr = np.math.floor(np.math.log10(lr))  
+        # print(f"Old lr = {lr}, lr order = {order_lr}")
+        
+        # Calculate mean of a batch of loss values
+        Lm = np.mean(loss_metrics)
+
+        # Calculate the normalized slope - Observations
+        Norm = (loss_metrics[-1] - loss_metrics[0]) / Lm
+        # print(f"Obs = {Norm} ")
+
+        if Norm == 0:
+            return lr
+
+        if Norm > 2: # this means, if Norm is > 0
+            # This means the loss increased so decrease the lr          
+            Norm *= self.del_d * np.math.pow(10, order_lr)
+        elif Norm > 1e-8:
+            Norm *= np.math.pow(10, order_lr)
+        else:
+            # This means the loss is decreasing so gradually increase the lr
+            Norm *= self.del_i * np.math.pow(10, order_lr)
+
+        # update learning rate
+        lr -= Norm
+        # print(f"Norm = {Norm} \n Learning rate= {lr}")
+        lr = abs(lr)
+
+        if lr < self.lr_lower_threshold:
+            lr = self.lr_lower_threshold
+            # print(f"DLRS [info]: Lower limit reached, lr = {lr}")
+            return lr, True
+        elif lr > self.lr_upper_threshold:
+            lr = self.lr_upper_threshold
+            # print(f"DLRS [info]: Upper limit reached, lr = {lr}")
+            return lr, True
+        else:
+            order_lr = np.math.floor(np.math.log10(lr))
+            # print(f"DLRS [info]: New lr = {lr}, lr order = {order_lr}")
+            return lr, True
